21540a494579361691c5e96a9f1ba82b9e408c95
haddanas <nassim.hd@gmail.com>
2013-03-05 10:02:26 +0100

    minor changes

--- README.md
+++ README.md
@@ -11,3 +11,8 @@ To test it:
     setwd("./shiny-handsontable")
     library(shiny)
     runApp()
+
+Next step: control the table content from the server  
+Things to try to make it work:
+* dynamic UI: http://rstudio.github.com/shiny/tutorial/#dynamic-ui
+* input at the beginning
--- shiny-handsontable/minimal02/www/shiny-handsontable.js
+++ shiny-handsontable/minimal02/www/shiny-handsontable.js
@@ -12,7 +12,17 @@ var tableInputBinding = new Shiny.InputBinding();
       
       return data_encoded;
     },
-    setValue: function(el) {
+    setValue: function(el, value) {
+      
+      var newData = [
+        ["Year2", "Kia2", "Nissan2", "Toyota2", "Honda2"],
+        ["2008", 10, 11, 12, 13],
+        ["2009", 20, 11, 14, 13],
+        ["2010", 30, 15, 12, 13]
+        ];
+        
+        
+      $(el).handsontable('setData', newData);
     },
     subscribe: function(el, callback) {
       $(el).on('change.tableInputBinding', function(e) { callback(); });
